775b49746131f41a2684dbe89b06cc76db234dc5
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2025-01-22 15:53:07 +0530

    Backend for Patient Profile

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoute from "./routes/ProfileRoute.js";
+import getValueforBPRouter from "./routes/BloodPressure.js";
+import getValueforSugarLevelRouter from "./routes/SugarLevelRoute.js";
+import getValueforWeightRouter from "./routes/WeightRouter.js";
+import getPDFrouter from "./routes/PdfRoute.js";
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 7000;
+const URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("DB connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB connection failed:", error);
+  });
+
+
+app.use("/api/user", userRoute);
+app.use("/api/healthrecord",getValueforBPRouter);
+app.use("/api/healthrecord",getValueforSugarLevelRouter);
+app.use("/api/healthrecord",getValueforWeightRouter);
+app.use("/api/pdfdetails", getPDFrouter);
